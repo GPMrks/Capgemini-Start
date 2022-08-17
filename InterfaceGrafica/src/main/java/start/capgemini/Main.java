7c80232547486fb31293ce1a07cf5b6562b29e8a
GPMrks <guilhermepereiramarques@hotmail.com>
2022-08-17 20:38:48 -0300

    ADD - Added practice projects - Java Swing/JFrame

---
+++ InterfaceGrafica/src/main/java/start/capgemini/Main.java
@@ -0,0 +1,7 @@
+package start.capgemini;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ InterfaceGrafica/src/main/java/start/capgemini/PrimeiraTela.java
@@ -0,0 +1,18 @@
+package start.capgemini;
+
+import javax.swing.*;
+
+public class PrimeiraTela {
+
+    private JTabbedPane tabbedPane1;
+    private JTextField textField1;
+    private JLabel nomeLabel;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton logarButton;
+    private JButton esqueciMinhaSenhaButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
